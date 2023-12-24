dafbbb8ccbaaf23b25c56f6704a6c72f0ec787a2
Gábor Závodszky <gzavodszky@gmail.com>
2023-12-24 13:28:57 +0100

    Merging Eleanor's update for Archer2

--- scripts/archer2_env.sh
+++ scripts/archer2_env.sh
@@ -4,21 +4,22 @@ if [[ "$0" == "$BASH_SOURCE" ]]; then
  exit 1
 fi
 
-
-module load cmake/3.21.3
-module load cray-hdf5/1.12.0.7
-module load cray-mpich/8.1.9
-module load cray-python/3.9.4.1
+module load cmake
+module load cray-hdf5
+module load cray-python
 module list
 
-export CC=$(which clang)
-export CXX=$(which clang++)
-export LDFLAGS="-L${CRAYLIBS_X86_64} $(CC --cray-print-opts=libs) -lmpi"
+export CC=cc
+export CXX=CC
 
-# compile by first running: cmake ../ -DMPI_CXX_COMPILER=$(which CC) -DMPI_C_COMPILER=$(which cc) -DMPI_COMPILER_FLAGS="--cray-print-opts=all"
-# from hemocell/build directory
+# Compile by first running: cmake ..
+# 	from hemocell/build directory
 
-# NOTE:
+
+# NOTES
+#
+# Comment out line 130 in CMakeLists.txt to avoid messy unused parameter warnings.
+#
 # Improve the performnce of HemoCell by adding the following options to srun:
 #
 # srun --distribution=block:block --hint=nomultithread "$example" config.xml
